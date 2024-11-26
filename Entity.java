6272bfae365220d8a0daafc676eb2115057bfc3f
Sophie Rocco <srocco@guilford.edu>
2024-11-26 18:18:26 -0500

    MY LITTLE GUY MOVES???? CUTEEEE

--- Entity.java
+++ Entity.java
@@ -1,5 +1,10 @@
+import java.awt.image.BufferedImage;
+
 public class Entity {
     public int x;
     public int y;
     public int speed;
+
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public String direction;
 }
--- Player.java
+++ Player.java
@@ -1,6 +1,9 @@
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.plaf.basic.BasicComboBoxUI.KeyHandler;
 
 public class Player extends Entity{
@@ -13,28 +16,66 @@ public class Player extends Entity{
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
     }
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
+    }
+    public void getPlayerImage() {
+        try {
+            // up1 = ImageIO.read(Player.class.getResource("/blackcat_backward_leftpaw.png"));
+            up1 = ImageIO.read(getClass().getResourceAsStream("/res/player/blackcat_backward_leftpaw.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/res/player/blackcat_backward_rightpaw.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/res/player/blackcat_forward_leftpaw.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/res/player/blackcat_forward_rightpaw.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/res/player/blackcat_left_leftpaw.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/res/player/blackcat_left_rightpaw.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/res/player/blackcat_right_leftpaw.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/res/player/blackcat_right_rightpaw.png"));
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
     }
     public void update() {
         if(keyH.upPressed == true){
             y = y - speed;
+            direction = "up";
         }
         if(keyH.downPressed == true){
             y = y + speed;
+            direction = "down";
         }
         if(keyH.leftPressed == true){
             x = x - speed;
+            direction = "left";
         }
         if(keyH.rightPressed == true){
             x = x + speed;
+            direction = "right";
         }
     }
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.white);
-        g2.fillRect(x,y,gamePanel.tileSize,gamePanel.tileSize); // Mock player character
+        BufferedImage image = null;
+        switch(direction) {
+            case "up":
+                image = up1;
+                break;
+            case "down":
+                image = down1;
+                break;
+            case "left":
+                image = left1;
+                break;
+            case "right":
+                image = right1;
+                break;
+        }
+        g2.drawImage(image, x, y, gamePanel.tileSize, gamePanel.tileSize,null);
+    
+        // g2.setColor(Color.white);
+        // g2.fillRect(x,y,gamePanel.tileSize,gamePanel.tileSize); // Mock player character
     }
 }
