9e083bafe43a7115d9238c435f6ef09c32739374
Sophie Rocco <srocco@guilford.edu>
2024-11-26 18:44:42 -0500

    my baby moves im in tears

--- Player.java
+++ Player.java
@@ -56,21 +56,51 @@ public class Player extends Entity{
             x = x + speed;
             direction = "right";
         }
+
+        spriteCounter ++; // Animation changes every 10 frames
+        if(spriteCounter > 10){
+            if(spriteNum == 1){
+                spriteNum = 2;
+            }else if(spriteNum == 2){
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
+        }
     }
     public void draw(Graphics2D g2) {
         BufferedImage image = null;
         switch(direction) {
             case "up":
-                image = up1;
+                if(spriteNum == 1){
+                    image = up1;
+                }
+                if(spriteNum == 2){
+                    image = up2;
+                }
                 break;
             case "down":
-                image = down1;
+                if(spriteNum == 1){
+                    image = down1;
+                }
+                if(spriteNum == 2){
+                    image = down2;
+                }                
                 break;
             case "left":
-                image = left1;
+                if(spriteNum == 1){
+                    image = left1;
+                }
+                if(spriteNum == 2){
+                    image = left2;
+                }                
                 break;
             case "right":
-                image = right1;
+                if(spriteNum == 1){
+                    image = right1;
+                }
+                if(spriteNum == 2){
+                    image = right2;
+                }                
                 break;
         }
         g2.drawImage(image, x, y, gamePanel.tileSize, gamePanel.tileSize,null);
